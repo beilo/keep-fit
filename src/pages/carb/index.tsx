6de0caa9c1459709de4b0534153deabf0fa18094
雷鹏 <leipeng@codemao.cn>
2022-08-17 17:51:30 +0800

    mobx移除后代码更改

--- src/pages/carb/index.tsx
+++ src/pages/carb/index.tsx
@@ -1,143 +1,103 @@
 import {
-    ActionSheet,
-    Button,
-    Cell,
-    CellGroup,
-    Field,
-    Grid,
-    GridItem,
-    Stepper
+  ActionSheet,
+  Button,
+  Cell,
+  CellGroup,
+  Field,
+  Grid,
+  GridItem,
+  Stepper,
 } from "@antmjs/vantui";
 import { View } from "@tarojs/components";
 import Taro, { useDidShow } from "@tarojs/taro";
-import { foodList2ActionSheetActions, FOOD_LIST } from "./constant";
+import { useSnapshot } from "valtio";
+import { foodList2ActionSheetActions } from "./constant";
+import { action, state } from "./store";
 
-function Carb() {
-  const store = useLocalStore(() => ({
-    kg: undefined,
-    carbMultiple: 2,
-    setKgCarbMultiple(kg: number | null, carbMultiple: number | null) {
-      if (kg) {
-        this.kg = kg;
-      }
-      if (carbMultiple !== undefined && carbMultiple !== null) {
-        this.carbMultiple = carbMultiple;
-      }
-    },
-    visFoodSheet: false,
-    setVisFoodSheet(bool: boolean) {
-      this.visFoodSheet = bool;
-    },
-    curFoodTag: "zao" as "zao" | "zhong" | "wan",
-    setCurFoodTag(tag: "zao" | "zhong" | "wan") {
-      this.curFoodTag = tag;
-    },
-    food: [FOOD_LIST[0], FOOD_LIST[0], FOOD_LIST[0]] as TFood[],
-    setFood(food: TFood) {
-      if (this.curFoodTag === "zao") this.food[0] = food;
-      if (this.curFoodTag === "zhong") this.food[1] = food;
-      if (this.curFoodTag === "wan") this.food[2] = food;
-    },
-    setAllFood(arr: TFood[]) {
-      this.food = arr;
-    },
-
-    // computed
-    get sumCarb() {
-      return this.kg * this.carbMultiple;
-    },
-    get zaoFoodWeight() {
-      return (((this.sumCarb * 0.5) / this.food[0].carb) * 100).toFixed();
-    },
-    get startFoodWeight() {
-      return (((this.sumCarb * 0.3) / this.food[1].carb) * 100).toFixed();
-    },
-    get endFoodWeight() {
-      return (((this.sumCarb * 0.2) / this.food[2].carb) * 100).toFixed();
-    },
-  }));
+export default function Carb() {
+  const snap = useSnapshot(state);
 
   useDidShow(() => {
-    store.setKgCarbMultiple(
+    action.setKgCarbMultiple(
       Number(Taro.getStorageSync("kg")),
       Number(Taro.getStorageSync("carbMultiple")) || 2
     );
     let locFood = Taro.getStorageSync("food");
     if (locFood) {
-      store.setAllFood(JSON.parse(locFood));
+      action.setAllFood(JSON.parse(locFood));
     }
   });
   const save2Storage = () => {
-    Taro.setStorageSync("kg", store.kg);
-    Taro.setStorageSync("carbMultiple", store.carbMultiple);
-    Taro.setStorageSync("food", JSON.stringify(store.food));
+    Taro.setStorageSync("kg", snap.kg);
+    Taro.setStorageSync("carbMultiple", snap.carbMultiple);
+    Taro.setStorageSync("food", JSON.stringify(snap.food));
   };
 
   return (
     <>
       <CellGroup>
         <Field
-          value={store.kg}
-          label='体重(kg)'
-          placeholder='请输入体重(kg)'
+          value={snap.kg}
+          label="体重(kg)"
+          placeholder="请输入体重(kg)"
           border={false}
-          type='number'
+          type="number"
           onChange={(e) => {
-            store.setKgCarbMultiple(Number(e.detail || null), null);
+            action.setKgCarbMultiple(Number(e.detail || null), null);
           }}
           renderButton={
-            <Button plain type='primary' onClick={save2Storage}>
+            <Button plain type="primary" onClick={save2Storage}>
               保存数据在本地
             </Button>
           }
         />
         <Cell
-          title='动态碳水'
+          title="动态碳水"
           renderExtra={
             <Stepper
-              min='1'
-              max='6'
-              value={store.carbMultiple}
+              min="1"
+              max="6"
+              value={snap.carbMultiple}
               asyncChange
               onChange={(event) => {
                 console.log("stepper", event);
 
-                store.setKgCarbMultiple(null, Number(event.detail));
+                action.setKgCarbMultiple(null, Number(event.detail));
               }}
             />
           }
         />
         <Cell
           onClick={() => {
-            store.setVisFoodSheet(true);
-            store.setCurFoodTag("zao");
+            action.setVisFoodSheet(true);
+            action.setCurFoodTag("zao");
           }}
-          title='早餐'
-          renderExtra={<View>{store.food[0].name}</View>}
+          title="早餐"
+          renderExtra={<View>{snap.food[0].name}</View>}
         />
         <Cell
           onClick={() => {
-            store.setVisFoodSheet(true);
-            store.setCurFoodTag("zhong");
+            action.setVisFoodSheet(true);
+            action.setCurFoodTag("zhong");
           }}
-          title='练前餐'
-          renderExtra={<View>{store.food[1].name}</View>}
+          title="练前餐"
+          renderExtra={<View>{snap.food[1].name}</View>}
         />
         <Cell
           onClick={() => {
-            store.setVisFoodSheet(true);
-            store.setCurFoodTag("wan");
+            action.setVisFoodSheet(true);
+            action.setCurFoodTag("wan");
           }}
-          title='练后餐'
-          renderExtra={<View>{store.food[2].name}</View>}
+          title="练后餐"
+          renderExtra={<View>{snap.food[2].name}</View>}
         />
       </CellGroup>
       <ActionSheet
-        show={store.visFoodSheet}
+        show={snap.visFoodSheet}
         actions={foodList2ActionSheetActions()}
-        onClose={() => store.setVisFoodSheet(false)}
+        onClose={() => action.setVisFoodSheet(false)}
         onSelect={(event) => {
-          store.setFood(event.detail);
+          action.setFood(event.detail);
         }}
       />
 
@@ -145,24 +105,22 @@ function Carb() {
         <GridItem>
           早餐
           <View>
-            {store.food[0].name}: {store.zaoFoodWeight}g
+            {snap.food[0].name}: {snap.zaoFoodWeight()}g
           </View>
         </GridItem>
         <GridItem>
           练前餐
           <View>
-            {store.food[1].name}: {store.startFoodWeight}g
+            {snap.food[1].name}: {snap.startFoodWeight()}g
           </View>
         </GridItem>
         <GridItem>
           练后餐
           <View>
-            {store.food[2].name}: {store.endFoodWeight}g
+            {snap.food[2].name}: {snap.endFoodWeight()}g
           </View>
         </GridItem>
       </Grid>
     </>
   );
 }
-
-export default observer(Carb);
--- src/pages/carb/store.ts
+++ src/pages/carb/store.ts
@@ -1,55 +1,50 @@
-import { proxy, useSnapshot } from "valtio";
-import { derive } from "valtio/utils";
+import { proxy } from "valtio";
 import { FOOD_LIST } from "./constant";
 
-const state = proxy({
-  kg: 0,
-  carbMultiple: 2,
+class State {
+  kg = 0;
+  carbMultiple = 2;
 
-  visFoodSheet: false,
-  curFoodTag: "zao" as "zao" | "zhong" | "wan",
-  food: [FOOD_LIST[0], FOOD_LIST[0], FOOD_LIST[0]] as TFood[],
-
-  get zaoFoodWeight() {
-    return (((state.sumCarb * 0.5) / state.food[0].carb) * 100).toFixed();
-  },
-  get startFoodWeight() {
-    return (((state.sumCarb * 0.3) / state.food[1].carb) * 100).toFixed();
-  },
-  get endFoodWeight() {
-    return (((state.sumCarb * 0.2) / state.food[2].carb) * 100).toFixed();
-  },
-});
-
-export const derived  = derive({
-    sumCarb: (get) => get(state).kg * get(state).carbMultiple,
-    zaoFoodWeight: (get) => (((derived.sumCarb * 0.5) / state.food[0].carb) * 100).toFixed();
-
-});
-
-export function setKgCarbMultiple(
-  kg: number | null,
-  carbMultiple: number | null
-) {
-  if (kg) {
-    state.kg = kg;
+  visFoodSheet = false;
+  curFoodTag: "zao" | "zhong" | "wan" = "zao";
+  food = [FOOD_LIST[0], FOOD_LIST[0], FOOD_LIST[0]] as TFood[];
+  sumCarb() {
+    return this.kg * this.carbMultiple;
   }
-  if (carbMultiple !== undefined && carbMultiple !== null) {
-    state.carbMultiple = carbMultiple;
+  zaoFoodWeight() {
+    return (((this.sumCarb() * 0.5) / this.food[0].carb) * 100).toFixed();
+  }
+  startFoodWeight() {
+    return (((this.sumCarb() * 0.3) / this.food[1].carb) * 100).toFixed();
+  }
+  endFoodWeight() {
+    return (((this.sumCarb() * 0.2) / this.food[2].carb) * 100).toFixed();
   }
 }
 
-export function setVisFoodSheet(bool: boolean) {
-  state.visFoodSheet = bool;
-}
-export function setCurFoodTag(tag: "zao" | "zhong" | "wan") {
-  state.curFoodTag = tag;
-}
-export function setFood(food: TFood) {
-  if (state.curFoodTag === "zao") state.food[0] = food;
-  if (state.curFoodTag === "zhong") state.food[1] = food;
-  if (state.curFoodTag === "wan") state.food[2] = food;
-}
-export function setAllFood(arr: TFood[]) {
-  state.food = arr;
-}
+export const state = proxy(new State());
+
+export const action = {
+  setKgCarbMultiple(kg: number | null, carbMultiple: number | null) {
+    if (kg) {
+      state.kg = kg;
+    }
+    if (carbMultiple !== undefined && carbMultiple !== null) {
+      state.carbMultiple = carbMultiple;
+    }
+  },
+  setVisFoodSheet(bool: boolean) {
+    state.visFoodSheet = bool;
+  },
+  setCurFoodTag(tag: "zao" | "zhong" | "wan") {
+    state.curFoodTag = tag;
+  },
+  setFood(food: TFood) {
+    if (state.curFoodTag === "zao") state.food[0] = food;
+    if (state.curFoodTag === "zhong") state.food[1] = food;
+    if (state.curFoodTag === "wan") state.food[2] = food;
+  },
+  setAllFood(arr: TFood[]) {
+    state.food = arr;
+  },
+};
