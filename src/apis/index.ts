dfec1aea21c93b71cbccbf44f22f7501777336e6
雷鹏windows <leipeng950504@gmail.com>
2022-08-21 22:36:37 +0800

    我的页面

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -8,4 +8,23 @@ export const Http = axios.create({
   adapter: TaroAdapter, // add this line，添加这一行使用taroAdapter
 });
 
+// interceptors for response
+Http.interceptors.response.use(
+  function (response) {
+    if (!response) {
+      return Promise.reject(new Error("response为空，请检查网络"));
+    }
+    if (!response.data) {
+      return Promise.reject(new Error("response.data为空，服务器异常"));
+    }
+    if (typeof response.data.code !== 'number') {
+      return Promise.reject(new Error("code异常，服务器异常"));
+    }
+    return response.data;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 export { AxiosPromise } from "axios";
--- src/app.config.ts
+++ src/app.config.ts
@@ -2,13 +2,13 @@ import { ROUTE_PATHS } from "./router";
 
 export default defineAppConfig({
   pages: [
-    ROUTE_PATHS["my-info"],
     ROUTE_PATHS.home,
     ROUTE_PATHS.aa,
     ROUTE_PATHS["ledger-list"],
     ROUTE_PATHS["invite-bill"],
     ROUTE_PATHS["empty-bill"],
     ROUTE_PATHS["add-bill"],
+    ROUTE_PATHS["my-info"],
 
     ROUTE_PATHS.carb,
   ],
