0768e3b2f3ae4652217e979d3ccaabd8755d1562
雷鹏windows <leipeng950504@gmail.com>
2022-10-22 17:11:17 +0800

    删除增加弹窗

--- src/pages/aa/components/pop-detail/index.tsx
+++ src/pages/aa/components/pop-detail/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Cell, CellGroup, Popup } from '@antmjs/vantui'
+import { Button, Cell, CellGroup, Dialog, Popup } from '@antmjs/vantui'
 import { View } from '@tarojs/components';
 import classnames from 'classnames/bind'
+import { useCallback } from 'react';
 import styles from './index.module.less'
 const cx = classnames.bind(styles);
 
@@ -14,41 +15,54 @@ interface IProps {
 export function PopDetail({
     bill, vis, onClose, onDel, onEdit
 }: IProps) {
-
-    return <Popup show={vis} position='bottom' round safeAreaInsetBottom onClose={onClose}>
-        <View className={cx('wrap')}>
-            <CellGroup title="消费" inset>
-                <Cell title="日常" border={false} value={bill?.billAmount || 0} />
-            </CellGroup>
-            <CellGroup title='付款人' inset>
-                {bill?.payers?.map(it => {
-                    return <Cell
+    const handleDel = useCallback(() => {
+        Dialog.confirm({
+            title: '标题',
+            message: "是否删除该账单",
+            selector: 'popDetailDialog',
+        }).then((it) => {
+            if (it === 'confirm') {
+                onDel()
+            }
+        })
+    }, [])
+    return <>
+        <Popup show={vis} position='bottom' round safeAreaInsetBottom onClose={onClose}>
+            <View className={cx('wrap')}>
+                <CellGroup title="消费" inset>
+                    <Cell title="日常" border={false} value={bill?.billAmount || 0} />
+                </CellGroup>
+                <CellGroup title='付款人' inset>
+                    {bill?.payers?.map(it => {
+                        return <Cell
+                            border={false}
+                            title={it.userName}
+                            value={it.amount}
+                        />
+                    })}
+                </CellGroup>
+                <CellGroup title='参与人' inset>
+                    {bill?.participants?.map(it => {
+                        return <Cell
+                            border={false}
+                            title={`${it.userName} 等 ${it.copiesNumber}人`}
+                            value={it.amount}
+                        />
+                    })}
+                </CellGroup>
+                {bill?.remarks && <CellGroup title='备注' inset>
+                    <Cell
                         border={false}
-                        title={it.userName}
-                        value={it.amount}
+                        title={bill.remarks}
                     />
-                })}
-            </CellGroup>
-            <CellGroup title='参与人' inset>
-                {bill?.participants?.map(it => {
-                    return <Cell
-                        border={false}
-                        title={`${it.userName} 等 ${it.copiesNumber}人`}
-                        value={it.amount}
-                    />
-                })}
-            </CellGroup>
-            {bill?.remarks && <CellGroup title='备注' inset>
-                <Cell
-                    border={false}
-                    title={bill.remarks}
-                />
-            </CellGroup>}
+                </CellGroup>}
 
-            <View className={cx('btn-wrap')}>
-                <Button className={cx('btn')} type="danger" plain onClick={onDel}>删除</Button>
-                <Button className={cx('btn')} type="primary" plain onClick={onEdit}>编辑</Button>
+                <View className={cx('btn-wrap')}>
+                    <Button className={cx('btn')} type="danger" plain onClick={handleDel}>删除</Button>
+                    <Button className={cx('btn')} type="primary" plain onClick={onEdit}>编辑</Button>
+                </View>
             </View>
-        </View>
-    </Popup >
+        </Popup >
+        <Dialog id="popDetailDialog" />
+    </>
 }
